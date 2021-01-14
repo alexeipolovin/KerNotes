b1ceb7ebc9024a869f2c56288f964df948c5d19e
Alexei Polovin <alexeipolovin@gmail.com>
2021-01-15 00:34:08 +0300

    +Simple web connector

--- webconnector.cpp
+++ webconnector.cpp
@@ -1,6 +1,39 @@
 #include "webconnector.h"
 
+#include <QtNetwork/QNetworkReply>
+
 WebConnector::WebConnector()
 {
 
+}
+
+void WebConnector::setServerUrl(const QUrl &value)
+{
+    serverUrl = value;
+}
+
+QUrl WebConnector::getServerUrl() const
+{
+    return serverUrl;
+}
+
+void WebConnector::sendRequest(QNetworkRequest &request, WebConnector::REQUEST_TYPE type)
+{
+    switch (type)
+    {
+        case WebConnector::CHECK_DATA_UPDATES:
+        {
+            manager->get(request);
+            // от безжалостных уличных драмм
+            connect(manager, &QNetworkReply::finished, )
+        }
+
+    }
+}
+
+QNetworkRequest* WebConnector::createRequest(WebConnector::REQUEST_TYPE type)
+{
+    QNetworkRequest *request = new QNetworkRequest();
+    request->setUrl(this->serverUrl.toString() + "?type=" + type);
+    return request;
 }
--- webconnector.h
+++ webconnector.h
@@ -4,10 +4,32 @@
 #include <QtNetwork/QNetworkAccessManager>
 #include <QUrl>
 
-class WebConnector
+class WebConnector : public QObject
 {
+private:
+
+    QNetworkAccessManager *manager;
+
+    QUrl serverUrl;
+
 public:
+    Q_OBJECT
+
     WebConnector();
+
+    enum REQUEST_TYPE {
+        CHECK_SELF_UPDATES,
+        CHECK_DATA_UPDATES,
+        UPDATE_SERVER_INFO,
+    };
+
+    QUrl getServerUrl() const;
+
+    void setServerUrl(const QUrl &value);
+
+    QNetworkRequest* createRequest(REQUEST_TYPE type);
+
+    void sendRequest(QNetworkRequest &request, REQUEST_TYPE type);
 };
 
 #endif // WEBCONNECTOR_H
